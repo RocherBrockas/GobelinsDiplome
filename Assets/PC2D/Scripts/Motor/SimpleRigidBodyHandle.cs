6b672361e1ee2da7cc4dc192369c2b3b952e9249
RocherBrockas <pierre.brossy@gmail.com>
2020-03-18 15:41:22 +0100

    Clean commit
    Clean Commit

---
+++ Assets/PC2D/Scripts/Motor/SimpleRigidBodyHandle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PC2D;
+
+public class SimpleRigidBodyHandle : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private float gravity = 9.81f;
+    public PerceptionTypes onlyInteractiveOnPerception;
+    public LayerMask expirationMask;
+
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        rigidbody.mass = 3;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PoofRange>() != null)
+        {
+            Debug.Log("oui");
+        }
+    }
+}
