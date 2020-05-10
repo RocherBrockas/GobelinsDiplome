0b06aba4c8512748893337ba8c2447ed8d854b99
RocherBrockas <pierre.brossy@gmail.com>
2020-05-10 22:33:23 +0200

    Working sprite shape dynamic modifier
    https://lesjoiesducode.fr/quand-je-dcouvre-une-api-qui-me-retourne-exactement-ce-que-je-veux

---
+++ Assets/spriteshapemodifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+
+public class spriteshapemodifier : MonoBehaviour
+{
+    public SpriteShapeController shape;
+    public int index;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        shape.spline.SetPosition( index, this.transform.position - shape.gameObject.transform.position);
+    }
+}
